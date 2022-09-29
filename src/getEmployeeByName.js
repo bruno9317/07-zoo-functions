444b3057fe54c615d3b77445abdfeee33653383c
bruno <brunobaptista9317@gmail.com>
2022-09-29 18:53:21 -0300

    salvando requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const empregados = data.employees;
+  const empregado = empregados.find((p) => p.firstName === employeeName);
+  const empregado2 = empregados.find((p) => p.lastName === employeeName);
+  if (typeof empregado !== 'undefined') {
+    return empregado;
+  }
+  if (typeof empregado2 !== 'undefined') {
+    return empregado2;
+  }
+  const objetoVazio = {};
+  return objetoVazio;
 }
 
 module.exports = getEmployeeByName;
