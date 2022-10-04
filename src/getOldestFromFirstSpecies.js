b2ba6c290f325527e6015094456ef07bbd646153
bruno <brunobaptista9317@gmail.com>
2022-10-04 17:02:23 -0300

    salvando requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+function ordenaDecrescente(a, b) {
+  return (b - a);
+}
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionario = data.employees.find((p) => p.id === id);
+  const idDaEspecie = funcionario.responsibleFor[0];
+  const dadosDaEspecie = data.species.find((p) => p.id === idDaEspecie);
+  const residentes = dadosDaEspecie.residents;
+  const idades = residentes.map((p) => p.age);
+  idades.sort(ordenaDecrescente);
+  const maiorIdade = idades[0];
+  const residenteMaisVelho = residentes.find((p) => p.age === maiorIdade);
+  return Object.values(residenteMaisVelho);
 }
 
 module.exports = getOldestFromFirstSpecies;
